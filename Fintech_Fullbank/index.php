467ac3c3dfe2c77c61e00920f38e78136f027900
PAULO CESAR RIBEIRO RODRIGUES PONTES <21192922@CFP127ALUNO.LOCAL>
2021-08-13 07:59:40 -0300

    Finalizado

--- Fintech_Fullbank/index.php
+++ Fintech_Fullbank/index.php
@@ -17,11 +17,17 @@ if(isset($_POST["nome"]) && isset($_POST["salario"])
     $genero = $_POST["genero"];
     $cargo = $_POST["cargo"];
 
-   
+   if($genero == "M"){
+       $Ogenero = "O funcionário";
+   }elseif($genero == "F"){
+    $Ogenero = "A funcionária";
+   }elseif($genero == "O"){
+    $Ogenero = "E funcionárie";
+   }
 
     if($salario > 5000){
        $aumentoSalarial = $salario + (($salario / 100) * 10);
-       echo "<h1>$nome passará a receber R$$aumentoSalarial, no cargo de $cargo</h1>";
+       echo "<h1>$Ogenero $nome passará a receber R$$aumentoSalarial, no cargo de $cargo</h1>";
     }else{
         $aumentoSalarial = $salario + (($salario / 100) * 20);
        echo "<h1>$nome passará a receber R$$aumentoSalarial, no cargo de $cargo</h1>";
